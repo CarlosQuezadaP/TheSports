6074cf47e45a7cc5793c370d37c96fcef6cb3376
Carlos Quezada <>
2021-04-09 15:42:03 -0500

    V1.0.0 - Feature : Se migra modulos gradle a gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+import appdependencies.Builds
+import appdependencies.Builds.APP_ID
+import appdependencies.Builds.BUILD_TOOLS
+import appdependencies.Builds.COMPILE_VERSION
+import appdependencies.Builds.MIN_VERSION
+import appdependencies.Builds.TARGET_VERSION
+import appdependencies.Versions
+import appdependencies.Plugins
+
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(COMPILE_VERSION)
+    buildToolsVersion = BUILD_TOOLS
+    defaultConfig {
+        applicationId = APP_ID
+        minSdkVersion(MIN_VERSION)
+        targetSdkVersion(TARGET_VERSION)
+        versionCode = Builds.App.VERSION_CODE
+        versionName = Builds.App.VERSION_NAME
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+
+
+}
+
+dependencies {
+
+    implementation(appdependencies.Libs.stdlib)
+
+    implementation(appdependencies.Libs.constraintlayout)
+    implementation(appdependencies.Libs.androidx_core)
+    implementation(appdependencies.Libs.material)
+    implementation(appdependencies.Libs.appcompat)
+
+    testImplementation("junit:junit:4.+")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
