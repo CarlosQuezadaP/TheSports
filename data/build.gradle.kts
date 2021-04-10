7f0a83580753f419d9958808981bc48fc80c60d4
Carlos Quezada <>
2021-04-09 20:10:05 -0500

    V1.0.0 - Feature : Creación de módulos de proyecto.

---
+++ data/build.gradle.kts
@@ -0,0 +1,43 @@
+import appdependencies.Builds
+import appdependencies.Builds.BUILD_TOOLS
+import appdependencies.Builds.COMPILE_VERSION
+import appdependencies.Builds.MIN_VERSION
+import appdependencies.Builds.TARGET_VERSION
+import appdependencies.Versions
+
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(COMPILE_VERSION)
+    buildToolsVersion = BUILD_TOOLS
+    defaultConfig {
+        minSdkVersion(MIN_VERSION)
+        targetSdkVersion(TARGET_VERSION)
+        versionCode = Builds.App.VERSION_CODE
+        versionName = Builds.App.VERSION_NAME
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+
+
+}
+
+dependencies {
+
+    implementation(appdependencies.Libs.stdlib)
+
+
+
+}
