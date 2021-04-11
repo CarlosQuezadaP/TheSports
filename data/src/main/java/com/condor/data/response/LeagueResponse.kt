a42b4cd2461862ef20628a37ae1e5d8cd454fe23
Carlos Quezada <>
2021-04-11 15:57:38 -0500

    v1.0.0 Feature : Creacion de responses

---
+++ data/src/main/java/com/condor/data/response/LeagueResponse.kt
@@ -0,0 +1,5 @@
+package com.condor.data.response
+
+import com.condor.data.dto.LeagueDto
+
+data class LeagueResponse(val leagueDtos: List<LeagueDto>)
---
+++ data/src/main/java/com/condor/data/response/ResponseEvent.kt
@@ -0,0 +1,5 @@
+package com.condor.data.response
+
+import com.condor.data.dto.EventDto
+
+data class ResponseEvent(val events: List<EventDto>)
